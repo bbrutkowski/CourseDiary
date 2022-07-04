f30333adc8a355bdad6415ac55f0bcdabbfaca04
boro <boromiro64@gmail.com>
2022-07-04 21:06:42 +0200

    [task] Add trainer

--- CourseDiary/CourseDiary.Domain/Models/Trainer.cs
+++ CourseDiary/CourseDiary.Domain/Models/Trainer.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CourseDiary.Domain.Models
 {
-    internal class Trainer
+    public class Trainer
     {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string DateOfBirth { get; set; }
     }
 }
--- CourseDiary/CourseDiary/Program.cs
+++ CourseDiary/CourseDiary/Program.cs
@@ -1,16 +1,58 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using CourseDiary.Domain;
+using CourseDiary.Domain.Models;
+using CourseDiary.Infrastructure;
+using System;
 using System.Threading.Tasks;
 
 namespace CourseDiary
 {
     internal class Program
     {
+        private TrainerService _trainerService;
+
+        public Program()
+        {
+            _trainerService = new TrainerService(new TrainerRepository());
+        }
+        
         static void Main(string[] args)
         {
+            new Program().Run();          
+        }
 
+        private void Run()
+        {
+            Console.WriteLine("Hello! What do you want to do?");
+            string action = Console.ReadLine();
+
+            switch (action)
+            {
+                case "Add trainer":
+                    AddTrainer();
+                        break;
+                default:
+                    Console.WriteLine("unsupported option");
+                    break;
+            }
+        }
+
+        public async void AddTrainer()
+        {
+
+            Trainer newTrainer = new Trainer
+            {
+                Name = Console.ReadLine(),
+                Surname = Console.ReadLine(),
+                Password = Console.ReadLine(),
+                Email = Console.ReadLine(),
+                DateOfBirth = Console.ReadLine(),
+            };
+
+            var isAdded = await _trainerService.AddTrainer(newTrainer);
+            if (isAdded == true)
+            {
+                Console.WriteLine("Trainer added successfully");
+            }
         }
     }
 }
