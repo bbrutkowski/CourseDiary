c05860559f018e3b03b8933c1e60c742e9552af5
Dominik <ddunst7@gmail.com>
2022-07-05 17:14:43 +0200

    [task] adding student table to sql

---
+++ CourseDiary/CourseDiary.Domain/Interfaces/IStudentRepository.cs
@@ -0,0 +1,15 @@
+﻿using CourseDiary.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseDiary.Domain.Interfaces
+{
+    public interface IStudentRepository
+    {
+        Task AddStudentAsync(Student student);
+        Task<List<Student>> GetAllStudentsAsync();
+    }
+}
---
+++ CourseDiary/CourseDiary.Domain/StudentService.cs
@@ -0,0 +1,34 @@
+﻿using CourseDiary.Domain.Interfaces;
+using CourseDiary.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseDiary.Domain
+{
+    public interface IStudentService
+    {
+        Task AddStudentAsync(Student student);
+    }
+
+    public class StudentService : IStudentService
+    {
+        private IStudentRepository _studentRepository;
+        public StudentService(IStudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public async Task AddStudentAsync(Student student)
+        {
+            await _studentRepository.AddStudentAsync(student);
+        }
+
+        public async Task<List<Student>> GetAllStudentsAsync()
+        {
+            return await _studentRepository.GetAllStudentsAsync();
+        }
+    }
+}
