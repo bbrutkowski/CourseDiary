cd05fe259a1baaa3de769aa7e08cc45347795475
Dominik <ddunst7@gmail.com>
2022-07-05 19:45:04 +0200

    [task] add trainer dash

--- CourseDiary/CourseDiary.TrainerApp.Dashboard/TrainerActionsHandler.cs
+++ CourseDiary/CourseDiary.TrainerApp.Dashboard/TrainerActionsHandler.cs
@@ -8,9 +8,45 @@ namespace CourseDiary.TrainerApp.Dashboard
 {
     internal class TrainerActionsHandler
     {
-        internal void ProgramLoop(string loggedUser)
+        private readonly CliHelper _cliHelper;
+        public bool ProgramLoop(string loggedUser)
         {
-            throw new NotImplementedException(); 
+            bool exit = false;
+
+            while (!exit)
+            {
+                string operation = _cliHelper.GetStringFromUser("");
+
+                switch (operation)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "6":
+                        break;
+                    case "7":
+                        break;
+                    case "8":
+                        break;
+                    case "9":
+                        break;
+                    case "10":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Wrong number, try again");
+                        break;
+                }
+            }
+
+            return exit;
         }
     }
 }
